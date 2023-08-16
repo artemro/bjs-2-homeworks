2499a2c75159c431dce15c19c3fd9b5b381ef223
Artem Rozhdestvenskiy <artem.rozhdestvenskiy@gmail.com>
2023-08-16 22:27:43 +0300

    first task functions

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,5 +1,19 @@
 function getArrayParams(...arr) {
-
+  let min = arr[0];
+  let max = arr[0];
+  let avg = arr[0];
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+    if (arr[i] > max) {
+      max = arr[i];
+      continue;
+    }
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+  };
+  avg = parseFloat((sum / arr.length).toFixed(2)); 
   return { min: min, max: max, avg: avg };
 }
 
