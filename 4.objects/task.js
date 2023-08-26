2a24961022aa217b91eaf478e58a5b4cb615a4ed
Artem Rozhdestvenskiy <artem.rozhdestvenskiy@gmail.com>
2023-08-27 00:15:06 +0300

    task completed

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,46 @@
 function Student(name, gender, age) {
-  
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+  this.marks = [];
 }
 
+new Student ("Maria", "female", 18);
+new Student ("Artem", "male", 21);
+new Student ("Denis", "male", 25);
+
+
 Student.prototype.setSubject = function (subjectName) {
-  
+  this.subject = subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
+   if (this.marks) {
+    marks.forEach((item) => {
+        this.marks.push(item)});
+   }
 }
 
 Student.prototype.getAverage = function () {
-  
+    if (!this.marks) {
+        return 0;
+    } 
+    else {
+        let result = this.marks.reduce((acc,item, index, arr) => {
+            acc += item;
+            if (index === arr.length - 1) {
+                return acc / arr.length;
+            } 
+            else {
+                return acc;
+            }
+        }, 0)
+        return result;
+    }
 }
 
 Student.prototype.exclude = function (reason) {
-  
+  delete this.marks;
+  delete this.subject;
+  this.excluded = reason;
 }
