d1da8a4d651c2f9b1617c5b2ee2909533a49ed4d
Artem Rozhdestvenskiy <artem.rozhdestvenskiy@gmail.com>
2023-10-08 22:03:12 +0300

    Exceptions first task

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,16 @@
-﻿
+﻿function parseCount(number) {
+    if (isNaN(parseFloat(number))) {
+        throw new Error("Невалидное значение");
+    } 
+    else return parseFloat(number);
+}
+
+function validateCount(number) {
+    try {
+        parseCount(number);
+    }
+    catch (error) {
+        return error;
+    }
+    return parseCount(number);
+}
